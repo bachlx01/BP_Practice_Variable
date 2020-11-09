2b3a4ade64904460f8bea2eb7a6ad6b3ebd142ba
bachlx01 <73922823+bachlx01@users.noreply.github.com>
2020-11-09 12:02:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var i =10;
+var f = 20.5;
+var b = true;
+var s = "Hà Nội";
+
+document.write('i=' + i +'<br>');
+document.write('f=' + f +'<br>');
+document.write('b=' + b +'<br>');
+document.write('s=' + s +'<br>');
+
+var width = 20
+var height = 10
+var area = width * height
+document.write('Area = ' + area)
+
+var a = prompt('Nhập một số bất kỳ');
+var b = prompt('Nhập một số')
+var c = a%b;
+
+alert(c==0)
